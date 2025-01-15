5112780c2923f0bf8a6a47676e65c31733a54da1
ananya-gta <anannya.gta@gmail.com>
2025-01-15 16:21:13 +0530

    add Switch and Yield examples demonstrating switch expressions and class structure in Java

--- decisionmaking/Switch.java
+++ decisionmaking/Switch.java
@@ -2,10 +2,11 @@ package decisionmaking;
 
 public class Switch {
     public static void main(String[] args) {
+        // switch labels
         String day = "";
         int val = 1;
         String output = switch(val) {
-            case 1 -> "Monday";
+            case 1 -> "Monday"; // after arrow we put the return value and no break statemnet is required
             case 2 -> "Tuesday";
             default -> "None";
         };
---
+++ decisionmaking/Yield.java
@@ -0,0 +1,7 @@
+package decisionmaking;
+
+public class Yield {
+    public static void main(String[] args) {
+        
+    }
+}
