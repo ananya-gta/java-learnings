90b90aa7480b749cfa2bd77896a61de9df3c8fa3
ananya-gta <anannya.gta@gmail.com>
2025-01-22 16:35:59 +0530

    fix class naming for thread creation example in Java

--- multithreading/ThreadcreationThroughExtends.java
+++ multithreading/ThreadcreationThroughExtends.java
@@ -4,18 +4,18 @@ package multithreading;
 // 2. Extending Thread Class
 
 // Step - 1 Creat a class that extends 'Theead' class and override the run()
-class MultithreadingLearning extends Thread {
+class MultithreadingLearnings extends Thread {
     @Override
     public void run() {
         System.out.println("code executed by thread" + Thread.currentThread().getName());
     }
 }
 
-public class ThreadcreationThroughExtends {
+public class ThreadCreationThroughExtends {
     public static void main(String[] args) {
         System.out.println("Going inside main method: " + Thread.currentThread().getName());
         // Step-2 create an instance of the subclass of Thread class
-        MultithreadingLearning tobj = new MultithreadingLearning();
+        MultithreadingLearnings tobj = new MultithreadingLearnings();
         
         // Step-3 start the thread
         tobj.start(); // this will invoke the subclass' run method 
