313a6fffc72d931367c6d40dd8d24b176446fe50
ananya-gta <anannya.gta@gmail.com>
2025-02-25 14:40:19 +0530

    enhance CompletableFuture1 class to demonstrate additional CompletableFuture functions including thenApply

--- threadpool/CompletableFuture1.java
+++ threadpool/CompletableFuture1.java
@@ -13,15 +13,27 @@ public class CompletableFuture1 {
             ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 10, TimeUnit.HOURS, new ArrayBlockingQueue<>(10),
                     Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
 
+            // Completable future has 5 main functions
+            // 1. supplyAsync()
             CompletableFuture<String> asyncTask = CompletableFuture.supplyAsync(() -> {
                 return "task completed using completableFuture";
             }, executor);
             // since completable futre is a child of futre so its get method can be used
             System.out.println(asyncTask.get());
+
+            // 2. thenApply()
+            CompletableFuture<String> asyncTask1 = CompletableFuture.supplyAsync(() -> {
+                return "task completed using completableFuture";
+            }, executor)
+                    .thenApply((String val) -> {
+                        return val + ", also used thenApply()";
+                    });
+
+            // since completable futre is a child of futre so its get method can be used
+            System.out.println(asyncTask1.get());
         } catch (Exception e) {
             // TODO: handle exception
         }
 
-       
     }
 }
