297432c0a5911ffc765984472ae55518ab60518a
ananya-gta <anannya.gta@gmail.com>
2025-03-04 12:17:22 +0530

    add Comparator implementation in ComparableVSComparator to demonstrate sorting with custom comparator

--- collections/ComparableVSComparator.java
+++ collections/ComparableVSComparator.java
@@ -1,6 +1,7 @@
 package collections;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ComparableVSComparator {
     public static void main(String[] args) {
@@ -19,11 +20,18 @@ public class ComparableVSComparator {
 
         // compare and sort based on color in lexicogrphically desc order
         Arrays.sort(carObj, (Car obj1, Car obj2) -> obj2.carColor.compareTo(obj1.carColor));
-        // compare and sort based on name in lexicogrphically asc order
-        Arrays.sort(carObj, (Car obj1, Car obj2) -> obj1.carName.compareTo(obj2.carName));
+        
         for (Car car : carObj) {
             System.out.println(car.carName + " " + car.carColor);
         }
+        // ----------------- another way
+        Car2[] carObj2 = new Car2[3];
+        carObj2[0] = new Car2("BMW", "Black");
+        carObj2[1] = new Car2("Audi", "Red");
+        carObj2[2] = new Car2("Mercedes", "Blue");
+
+        Arrays.sort(carObj2, new Car2());
+        
 
     }
 }
@@ -35,5 +43,21 @@ class Car {
     Car (String name, String color) {
         this.carName = name;
         this.carColor = color;
+    }
+}
+
+class Car2 implements Comparator<Car2> {
+    String carName;
+    String carColor;
+    Car2(){}
+    Car2(String name, String color) {
+        this.carName = name;
+        this.carColor = color;
+    }
+
+    @Override
+    public int compare(Car2 obj1, Car2 obj2) {
+        // compare and sort based on name in lexicogrphically asc order
+        return obj1.carName.compareTo(obj2.carName);
     }
 }
